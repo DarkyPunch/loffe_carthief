e7242c366d5dd8e65b2c67ec5d42a92d06406d2f
Jonathan Joestar <42867468+DarkyPunch@users.noreply.github.com>
2020-10-09 01:22:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -18,7 +18,8 @@ Config.amountOfDropoff = 5 -- (amount of dropoffPoints)
 
 Config.dropoffPoints = { -- x,y,z = where you leave the vehicle, sx,sy,sz,sh = x,y,z for vehicle spawn + heading for vehicle
 	[1] = { x = -587.76, y = -1637.21, z = 19.89, sx = 382.21, sy = -1300.89, sz = 43.55, sh = 137.47, model = "t20"},
-	[2] = { x = -587.76, y = -1637.21, z = 19.89, sx = 28.84, sy = -67.57, sz = 61.88, sh = 113.85, model = "tempesta"},
-	[3] = { x = -587.76, y = -1637.21, z = 19.89, sx = 53.63, sy = -1877.22, sz = 21.51, sh = 338.81, model = "t20"}
-
+	[2] = { x = -587.76, y = -1637.21, z = 19.89, sx = 356.14, sy = -1700.45, sz = 48.3, sh = 113.85, model = "tempesta"},
+	[3] = { x = -587.76, y = -1637.21, z = 19.89, sx = 282.97, sy = -750.72, sz = 53.25, sh = 338.81, model = "pfister811"},
+	[4] = { x = -587.76, y = -1637.21, z = 19.89, sx = 53.63, sy = -1877.22, sz = 21.51, sh = 338.81, model = "pfister811"}
+	[5] = { x = -587.76, y = -1637.21, z = 19.89, sx = 53.63, sy = -1877.22, sz = 21.51, sh = 338.81, model = "pfister811"}
 }
